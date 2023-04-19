2aa8f1831aa00f940704eab36478156a51fadb47
Ian7140 <rlawnals7140@gmail.com>
2023-04-19 15:53:01 +0900

    before recap

--- src/server.js
+++ src/server.js
@@ -1,36 +1,23 @@
 import express from "express";
+const PORT = 4000;
 
 const app = express();
 
-const URLLogger = (req, res, next) => {
-  console.log("Path: ", req.path);
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+}
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
-const timeLogger = (req, res, next) => {
-  const now = new Date();
-  console.log(`Time: ${now.getFullYear()}.${now.getMonth()}.${now.getDate()}`);
-  next();
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
 };
+app.get("/", gossipMiddleware, handleHome);
 
-const protectorLogger = (req, res, next) => {
-  if (req.path === "/protected") {
-    return res.send("<h1>Forbidden</h1>");
-  }
-  next();
-};
+app.get("/", logger, handleHome);
 
-const secureLogger = (req, res, next) => {
-  if (req.protocol === "https") {
-    console.log("Secure ✅");
-  } else {
-    console.log("Insecure ❌");
-  }
-  next();
-};
-
-app.use(URLLogger, timeLogger, secureLogger, protectorLogger);
-app.get("/", (req, res) => res.send("<h1>Home!</h1>"));
-app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
-
-app.listen(() => `Listening!✅`);
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
