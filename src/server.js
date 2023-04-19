09e011b827aa1b8c8622dbd3ef4bd26d6904eaf1
Ian7140 <rlawnals7140@gmail.com>
2023-04-20 07:36:11 +0900

    mini-application

--- src/server.js
+++ src/server.js
@@ -2,21 +2,23 @@ import express from "node_modules/express";
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
-}
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const globalRouter = express.Router();
+const handleHome = (req,res) => res.send("Home");
+globalRouter.get("/", handleHome);
+const userRouter = express.Router();
+const handleEditUser = (req,res) => res.send("Edit Users");
+userRouter.get("/edit", handleEditUser);
+const videoRouter = express.Router();
+const handleWatchVideo = (req,res) => res.send("Watch Video");
 
-const handleHome = (req, res) => { //arrow function은 return을 포함하고 있음
-  return res.send("I love middlewares"); // I love middlewares를 보냄 
-};
-app.get("/", gossipMiddleware, handleHome);
 
-app.get("/", logger, handleHome);
+app.use("/",globalRouter);
+app.use("/videos",videoRouter);
+app.use("/users",userRouter);
+
 //routes = "/"
 //console.log("Hello") 는 function이 아님 statement이라고 함
 
@@ -29,4 +31,7 @@ app.listen(PORT, handleListening); //컴퓨터에서의 창문? 사용자와 소
 middleware = res 와 req 사이에 있는 것
 next() : 다음함수로 책임 떠넘기기?
 누군가 대답하기 전까지는 middleware임
+
+url과 controller 관리를 쉽게 해줌 - "mini-application"이랑 비슷한 개념
+작업중인 주제를 기반으로 url를 그룹화 해주는 역할
 */
